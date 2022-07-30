4fe8d83fef7233e479f6a480baff56e32d08b9cc
smucharla <mucharla@gmail.com>
2022-07-30 22:21:07 +0000

    Comminting the interactive script

---
+++ interactive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+clear
+echo -n "What is your first name?    : "
+read fname
+echo -n "What is your last name?     : "
+read lname
+
+echo "Hello $fname $lname Nice to meet you"
+
+
+
